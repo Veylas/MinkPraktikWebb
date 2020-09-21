05ad23da6049d4257c5154caca369e7d1053328e
Chris <manchristoffer@yahoo.dk>
2020-09-21 09:10:52 +0200

    Fleet is rendering
    Fleet is rendering data from database

--- ThyrrestrupFrontEnd/routes/pages.js
+++ ThyrrestrupFrontEnd/routes/pages.js
@@ -3,7 +3,7 @@ const authController = require('../controllers/auth');
 const mssql = require("mssql");
 const { Result } = require('odbc');
 //const { rows } = require('mssql');
-var vehicleList = [];
+
 
 
 const router = express.Router();
@@ -25,35 +25,30 @@ router.get('/contact', (req, res) => {
 });
 
 
-router.get('/fleet', (req, res) => {
-    console.log("dadsaw")
-    res.end()
-})
-
 var request = new mssql.Request();
-
 
 router.get("/fleet", (req, res)=>{
 request.query("select * from Vehicles", (err, result) =>{
+    var vehicleList = [];
     if(err){
     console.log("failed to query for vehicles: " + err)
     res.sendStatus(500)
     return
     }
-    
+    var vehicleList = [];
         for (var i = 0; i < result.recordset.length; i++){
+            
             var vehicle = {
                 'vehicleID' :result.recordset[i].vehicleID,
                 'type' :result.recordset[i].type,
                 'powerBILink' :result.recordset[i].powerBILink,
                 'personID' :result.recordset[i].personID,
             }
-            console.log(result.recordset[i].vehicleID)
             vehicleList.push(vehicle);
         }
+        
         res.render('fleet', {"vehicleList": vehicleList});
     })
-console.log(vehicleList)
 
 })
 var config = ({
